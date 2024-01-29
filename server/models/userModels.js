06081c0989aeb56a098325013288efc1cb604dbf
Laveesh Tomar <laveeshtomar.royalking@gmail.com>
2024-01-29 09:27:42 +0530

    Added real user schema

--- server/models/userModels.js
+++ server/models/userModels.js
@@ -2,21 +2,61 @@
 
   const {Schema} = mongoose;
 
-  const exampleSchema = new Schema({
-    id: {
-      type: String,
-      required: true,
-    },
-    name: {
-      type: String,
-      required: true,
-    },
+  const apexSchema = new mongoose.Schema({
     title: {
       type: String,
       required: true,
     },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String, // You might store image URLs or use a file storage solution
+      required: true,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    dislikes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    reviews: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User',
+        },
+        text: {
+          type: String,
+        },
+        date: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    dateCreated: {
+      type: Date,
+      default: Date.now,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
   });
+  
 
-const ExampleModel = mongoose.model('ExampleModel', exampleSchema);
+const ApexModel = mongoose.model('ApexModel', apexSchema);
 
-export default ExampleModel;
+export default ApexModel;
